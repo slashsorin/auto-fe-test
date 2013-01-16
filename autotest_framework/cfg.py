004f9f116ee6fcf0c4a98a3b86336ec0a13f3f64
slashsorin <sorin.dimo@yahoo.com>
2013-01-16 13:23:04 +0100

    environment variables added - jenkins

--- autotest_framework/cfg.py
+++ autotest_framework/cfg.py
@@ -5,16 +5,22 @@
 
     Copyright Issuu Aps Jun 15, 2012
 '''
+import SauceParams
+import os
+
+tp = SauceParams.GetParams()
+
 config={
-        'username' : "sorin1068",
-        'access-key' : "23962938-d716-43f6-acf5-7cf6d66937b4",
-        'host' : "ondemand.saucelabs.com",
-        'port' : 4444,
-        'base-url' : 'adpages-uat.issuu.com',
-        
-        'platforms' : [ #{"os": "Windows 2003", "browser": "iexplore", "browser-version": "8."}, 
-                        #{"os": "Windows 2008", "browser": "iexplore", "browser-version": "9."},
-                        #{"os": "Windows 2008", "browser": "googlechrome", "browser-version": ""},
-                        {"os": "Windows 2008", "browser": "firefox", "browser-version": "13"}
-                        ]
+        'username' : os.environ.get('SAUCE_USER_NAME'), #"issuu",
+        'access-key' : os.environ.get('SAUCE_API_KEY'), #"23a5b32a-6ab8-4866-8f1d-9a13c98348c2",
+        'host' : os.environ.get('SELENIUM_HOST'), #"ondemand.saucelabs.com",
+        'port' : os.environ.get('SELENIUM_PORT'), #4444,
+        'base-url' : os.environ.get('SELENIUM_DOMAIN'),
+
+        'platforms' :   [{
+                         "os": os.environ.get('SELENIUM_PLATFORM'),
+                         "browser": os.environ.get('SELENIUM_BROWSER'),
+                         "browser-version": os.environ.get('SELENIUM_VERSION')
+                         }
+                         ]
         }
