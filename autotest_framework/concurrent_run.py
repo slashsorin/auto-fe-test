3f760a04972cda49a32f2ebf99bc362aa08d1fed
slashsorin <sorin.dimo@yahoo.com>
2013-01-21 14:42:04 +0100

    add parallel run for reporting

--- autotest_framework/concurrent_run.py
+++ autotest_framework/concurrent_run.py
@@ -7,21 +7,24 @@ import string
 def test_fun(path):
     os.chdir(path)
     start = time.time()
-
     tests = glob.glob('Test*.py')
 
     processes = []
-
-    for test in tests:
-        x = string.rstrip(test, '.py')
-        processes.append(Popen('python -m unittest %s' % x, shell=True))
     
-    for process in processes:
-        process.wait()
-
+    for test in tests:
+        processes.append(Popen('python %s' %test, shell=True))
+        #x = string.rstrip(test, '.py')
+        #processes.append(Popen('python -m unittest %s' % x, shell=True))
+        
     print "*" * 50
     print "Time taken: %s minutes" % ((time.time() - start) /60)
     os.chdir('..')
+    
 
-for p in ['tests_pro_account','tests_free_account']:
+for p in ['tests_pro_account','tests_free_account', 'tests_insight']:
     test_fun(p)
+
+'''    
+for p in ['tests_insight']:
+    test_fun(p)
+'''
