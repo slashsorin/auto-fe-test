d40aba5f4cd427b1d94ae5af9cc6a823f6cc734b
slashsorin <sorin.dimo@yahoo.com>
2013-01-22 14:16:42 +0100

    environment

--- autotest_framework/SeleniumTestCase.py
+++ autotest_framework/SeleniumTestCase.py
@@ -1,51 +1,48 @@
 #-*- coding: utf-8 -*-
 '''
-    
+
     @author: Sorin
 
     Copyright Issuu Aps Jun 15, 2012
 '''
 
+import datetime, unittest
 from selenium import selenium
-from unittest import TestCase
 import cfg
-
-import EnvironmentParam
-
-tp = EnvironmentParam.GetParams()
 
 try:
     import json
 except ImportError:
     import simplejson as json
 
-class SeleniumTestCase(TestCase):
+class SeleniumTestCase(unittest.TestCase):
     __test__ = False
-
+    
     def setUp(self):
         try:
-			c = {"username": cfg.config['username'],
+            c = {"username": cfg.config['username'],
                  "access-key": cfg.config['access-key'],
                  "job-name": getattr(self, "_testMethodName", self.__class__.__name__).replace('_', ' ').capitalize(),
                  "os": self.os,
                  "browser": self.browser,
                  "browser-version": self.version,
-                 #need to check that the idle-timeout works !!!
-                 "idle-timeout": 300
+                 "command-timeout": 600000,
+                 "idle-timeout": 300000,
+                 "tags": str(datetime.datetime.now()),
                  }
-			
-			session = selenium(cfg.config['host'],
-                           cfg.config['port'],
-                           json.dumps(c),
-                           "http://" + cfg.config['base-url'])
-			
-			session.start()
+            
+            session = selenium(cfg.config['host'],
+                               cfg.config['port'],
+                               json.dumps(c),
+                               "http://" + cfg.config['base-url'])
+                
+            session.start()
             session.set_timeout(90000)
-            session.selenium.set_speed(self, 500)
             self.selenium = session
+        
         except AttributeError:
             pass
-
+    
     def tearDown(self):
         try:
             self.selenium.stop()
