3d9cfbf861dac90fd4c35adf3fa21eb34a1ddd5e
slashsorin <sorin.dimo@yahoo.com>
2013-01-22 14:14:48 +0100

    environment

--- autotest_framework/SeleniumTestCase.py
+++ autotest_framework/SeleniumTestCase.py
@@ -24,15 +24,6 @@ class SeleniumTestCase(TestCase):
 
     def setUp(self):
         try:
-			c = {
-					"username" : os.environ.get('SAUCE_USER_NAME'),
-					"access-key" : os.environ.get('SAUCE_API_KEY'),
-					"os": os.environ.get('SELENIUM_PLATFORM'),
-	                "browser": os.environ.get('SELENIUM_BROWSER'),
-	                "browser-version": os.environ.get('SELENIUM_VERSION')
-				}
-            #for platf in cfg.config['platforms']:
-            '''
 			c = {"username": cfg.config['username'],
                  "access-key": cfg.config['access-key'],
                  "job-name": getattr(self, "_testMethodName", self.__class__.__name__).replace('_', ' ').capitalize(),
@@ -42,20 +33,13 @@ class SeleniumTestCase(TestCase):
                  #need to check that the idle-timeout works !!!
                  "idle-timeout": 300
                  }
-			'''
 			
-			session = selenium(os.environ.get('SELENIUM_HOST'),
-								os.environ.get('SELENIUM_PORT'),
-								json.dumps(c),
-								"http://" + os.environ.get('SELENIUM_DOMAIN'))
-			
-			'''
-            session = selenium(cfg.config['host'],
+			session = selenium(cfg.config['host'],
                            cfg.config['port'],
                            json.dumps(c),
                            "http://" + cfg.config['base-url'])
-			'''
-            session.start()
+			
+			session.start()
             session.set_timeout(90000)
             session.selenium.set_speed(self, 500)
             self.selenium = session
