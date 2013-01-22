404c13faf3083fe66a0e27b27f9c3c86ff1e3a7f
slashsorin <sorin.dimo@yahoo.com>
2013-01-22 10:15:04 +0100

    unittest run mode

--- autotest_framework/concurrent_run.py
+++ autotest_framework/concurrent_run.py
@@ -12,9 +12,9 @@ def test_fun(path):
     processes = []
     
     for test in tests:
-        processes.append(Popen('python %s' %test, shell=True))
-        #x = string.rstrip(test, '.py')
-        #processes.append(Popen('python -m unittest %s' % x, shell=True))
+        #processes.append(Popen('python %s' %test, shell=True))
+        x = string.rstrip(test, '.py')
+        processes.append(Popen('python -m unittest %s' % x, shell=True))
         
     print "*" * 50
     print "Time taken: %s minutes" % ((time.time() - start) /60)
