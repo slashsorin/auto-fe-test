19c29306ae8131ffffd21e4c8f0ecd45cf4b2d11
slashsorin <sorin.dimo@yahoo.com>
2013-02-26 09:32:22 +0100

    new run

--- autotest_framework/concurrent_run.py
+++ autotest_framework/concurrent_run.py
@@ -12,19 +12,18 @@ def test_fun(path):
     processes = []
     
     for test in tests:
-        #processes.append(Popen('python %s' %test, shell=True))
-        x = string.rstrip(test, '.py')
-        processes.append(Popen('python -m unittest %s' % x, shell=True))
+        processes.append(Popen('python %s' %test, shell=True))
+        #x = string.rstrip(test, '.py')
+        #processes.append(Popen('python -m unittest %s' % x, shell=True))
         
     print "*" * 50
     print "Time taken: %s minutes" % ((time.time() - start) /60)
     os.chdir('..')
     
-
+'''
 for p in ['tests_pro_account','tests_free_account']:
     test_fun(p)
-
 '''    
-for p in ['tests_insight']:
+
+for p in ['tests_flypaper']:
     test_fun(p)
-'''
