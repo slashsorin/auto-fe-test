5ed93f0f9f31c6b284c3db9a068db73d19ea2326
slashsorin <sorin.dimo@yahoo.com>
2013-02-28 14:19:47 +0100

    aa

--- frontend_issuu_autotest_replica/autotest_framework/SeleniumTestCase.py
+++ frontend_issuu_autotest_replica/autotest_framework/SeleniumTestCase.py
@@ -48,15 +48,14 @@ class SeleniumTestCase(TestCase):
             if hasattr(self,'avoid_proxy'):
                 c['avoid-proxy'] = self.avoid_proxy
 			
-			for baseURL in cfg.config['base-url']:
-                session = selenium(cfg.config['host'],
-                                   cfg.config['port'],
-                                   json.dumps(c),
-                                   baseURL)
-                #"https://" + cfg.config['base-url'])
-                session.start()
-                session.set_timeout(cfg.config['session_timeout'])
-                self.selenium = session
+            session = selenium(cfg.config['host'],
+								cfg.config['port'],
+								json.dumps(c),
+								"https://" + cfg.config['base-url'])
+								
+            session.start()
+            session.set_timeout(cfg.config['session_timeout'])
+            self.selenium = session
         except AttributeError:
             pass
         except Exception, e:
