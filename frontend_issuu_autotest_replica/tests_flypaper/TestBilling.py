93df56769434551dc20b69db77a3e5258c21dce8
slashsorin <sorin.dimo@yahoo.com>
2013-03-05 20:00:58 +0100

    http default

--- frontend_issuu_autotest_replica/tests_flypaper/TestBilling.py
+++ frontend_issuu_autotest_replica/tests_flypaper/TestBilling.py
@@ -42,14 +42,17 @@ class TestBilling(SeleniumTestCase):
             
             #print self.__class__.__name__ + " passed!"       
             #sts.set_test_status(self.selenium.get_eval("selenium.sessionId"), passed=True)
+            self.selenium.get_eval("selenium.sessionId")
             
+        
         except AttributeError:
             pass
-        #except: # catch *all* exceptions
+        except: # catch *all* exceptions
+            self.selenium.get_eval("selenium.sessionId")
             #if  sys.exc_info()[1]:
                 #print self.__class__.__name__ + " failed!"
                 #sts.set_test_status(self.selenium.get_eval("selenium.sessionId"), passed=False)
-        
+                
 globals().update(make_platform_classes.make_platform_classes(TestBilling))
 
 if __name__ == '__main__':
